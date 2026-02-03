9a7633c5d1a0fcdb17ff8927559980a467564428
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-02-03 23:05:17 +0100

    feat(Builder): Sandwich class added with 3 different tests

--- src/creational/builder/MainBuilder.java
+++ src/creational/builder/MainBuilder.java
@@ -1,7 +1,33 @@
 package creational.builder;
 
+import java.util.ArrayList;
+
 public class MainBuilder {
     public static void main() {
+        ArrayList<String> legumesList = new ArrayList<>();
+        legumesList.add("laitue");
+        legumesList.add("oignon");
 
+        Sandwich sandwich1 = new SandwichBuilder()
+                .pain("baguette")
+                .viande("jambon")
+                .fromage("cheddar")
+                .legumes(legumesList)
+                .build();
+        System.out.println(sandwich1);
+
+        Sandwich sandwich2 = new SandwichBuilder()
+                .pain("ciabatta")
+                .grille(true)
+                .build();
+        System.out.println(sandwich2);
+
+        Sandwich sandwich3 = new SandwichBuilder()
+                .pain("complet")
+                .viande("poulet")
+                .sauce("ketchup")
+                .grille(false)
+                .build();
+        System.out.println(sandwich3);
     }
 }
