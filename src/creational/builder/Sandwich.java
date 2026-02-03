004c9c00026c8bcbf398291dfa39a07a5d1f6873
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-02-03 23:02:58 +0100

    feat(Builder): add Sandwich class with builder pattern implementation

--- src/creational/builder/Sandwich.java
+++ src/creational/builder/Sandwich.java
@@ -1,4 +1,33 @@
 package creational.builder;
 
+import java.util.ArrayList;
+
 public class Sandwich {
+    private String pain;
+    private String viande;
+    private String fromage;
+    private ArrayList<String> legumes;
+    private String sauce;
+    private Boolean grille;
+
+    Sandwich(SandwichBuilder sandwichBuilder) {
+        this.pain = sandwichBuilder.pain;
+        this.viande = sandwichBuilder.viande;
+        this.fromage = sandwichBuilder.fromage;
+        this.legumes = sandwichBuilder.legumes;
+        this.sauce = sandwichBuilder.sauce;
+        this.grille = sandwichBuilder.grille;
+    }
+
+    @Override
+    public String toString() {
+        return "Sandwich{" +
+                "pain='" + pain + '\'' +
+                ", viande='" + viande + '\'' +
+                ", fromage='" + fromage + '\'' +
+                ", legumes=" + legumes +
+                ", sauce='" + sauce + '\'' +
+                ", grille=" + grille +
+                '}';
+    }
 }
