3d8192cf85f893c3fa7f6a3cad2d28b1651f2ba6
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-02-06 18:16:32 +0100

    feat(Facade): IngredientsDisponibility service added

---
+++ src/structural/facade/IngredientsDisponibility.java
@@ -0,0 +1,7 @@
+package structural.facade;
+
+public class IngredientsDisponibility {
+    public void isIngredientAvailable() {
+        System.out.println("All ingredients are available");
+    }
+}
