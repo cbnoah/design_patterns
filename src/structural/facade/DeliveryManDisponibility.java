91c237cfb12edd7f8173e83b50a4134f287eff50
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-02-06 18:16:47 +0100

    feat(Facade): DeliveryManDisponibility service added

---
+++ src/structural/facade/DeliveryManDisponibility.java
@@ -0,0 +1,7 @@
+package structural.facade;
+
+public class DeliveryManDisponibility {
+    public void isDeliveryManAvailable() {
+        System.out.println("3 delivery men are available");
+    }
+}
